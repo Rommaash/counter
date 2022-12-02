d3a7422ec2e3aa4278344e2b328df9d25bc2c85b
Rommaash <Rommaash@yandex.ru>
2022-12-02 17:19:06 +0200

    branch to merge

--- Sprint3/ViewController.swift
+++ Sprint3/ViewController.swift
@@ -9,23 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var titleLabel: UILabel!
-    private var valueCount: Int = 0
     
-    @IBAction func changeButton(_ sender: Any) {
-        valueCount += 1
-        print(valueCount)
-        titleLabel.text = "Значение счётчика: \(valueCount)"
-    }
-    override func viewDidLoad() {
-        super.viewDidLoad()
-       titleLabel.layer.masksToBounds = true
-        titleLabel.layer.cornerRadius = 40
-        titleLabel.layer.borderWidth = 4
-        titleLabel.layer.borderColor = UIColor.systemMint.cgColor
-       
-    }
+   @IBOutlet weak var titleLabel: UILabel!
+private var valueCount: Int = 0
 
-
+@IBAction func changeButton(_ sender: Any) {
+    valueCount += 1
+    print(valueCount)
+    titleLabel.text = "Значение счётчика: \(valueCount)"
+ }
+     override func viewDidLoad() {
+         super.viewDidLoad()
+        titleLabel.layer.masksToBounds = true
+         titleLabel.layer.cornerRadius = 40
+         titleLabel.layer.borderWidth = 4
+         titleLabel.layer.borderColor = UIColor.systemMint.cgColor
+      
+     }
+ 
+ 
 }
 
