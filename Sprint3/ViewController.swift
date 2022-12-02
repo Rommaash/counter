0655b08a0f158e2a2b77b000c55171e72182dc18
Rommaash <Rommaash@yandex.ru>
2022-12-02 14:40:50 +0200

    counter created

--- Sprint3/ViewController.swift
+++ Sprint3/ViewController.swift
@@ -9,9 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var titleLabel: UILabel!
+    private var valueCount: Int = 0
+    
+    @IBAction func changeButton(_ sender: Any) {
+        valueCount += 1
+        print(valueCount)
+        titleLabel.text = "Значение счётчика: \(valueCount)"
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       titleLabel.layer.masksToBounds = true
+        titleLabel.layer.cornerRadius = 40
+        titleLabel.layer.borderWidth = 4
+        titleLabel.layer.borderColor = UIColor.systemMint.cgColor
+       
     }
 
 
